1b453b16ac613af4325b1c8420c7085160d0b6cc
Chuwei Guo <chuwei0807@gmail.com>
2021-02-05 23:52:36 -0800

    added beautiful pics of chuwei

--- FaceRecognition.py
+++ FaceRecognition.py
@@ -1,3 +1,10 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+
+img = cv2.imread("chuwei2.jpg",cv2.IMREAD_GRAYSCALE)
+#IMREAD_COLOR
+#IMREAD_UNCHANGED
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
